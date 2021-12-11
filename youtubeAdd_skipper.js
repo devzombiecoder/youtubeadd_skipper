0ae66db71caece6c065960acca6a9c9864ca0c97
sameer choudhary <95902961+IAMsameerChoudhary@users.noreply.github.com>
2021-12-11 15:37:06 +0530

    Add files via upload

---
+++ youtubeAdd_skipper.js
@@ -0,0 +1,19 @@
+function __(a){return document.getElementsByClassName(a);}
+	console.log("started");
+	setInterval(function(){
+		prev_playback_speed=document.getElementsByTagName("video")[0].playbackRate;
+	    if(__("ytp-ad-text").length!=0){//if ad
+	        document.getElementsByTagName("video")[0].playbackRate = 16;//play video on 16x speed
+	    }else{
+	        document.getElementsByTagName("video")[0].playbackRate = prev_playback_speed;//play video on normal speed
+	    }
+	    if(__("ytp-ad-skip-button-text")[0]!=undefined){//if skip button is enabled
+	        __("ytp-ad-skip-button-text")[0].click();
+	    }else{
+	    }
+	},100)
+function speedController_(e){
+    if(e.keyCode==107 || e.keyCode==187){document.getElementsByTagName("video")[0].playbackRate += .25;}else if(e.keyCode==109 || e.keyCode==189){document.getElementsByTagName("video")[0].playbackRate -= .25;}
+document.getElementById("logo-icon").innerHTML=document.getElementsByTagName("video")[0].playbackRate+" x"
+}
+document.addEventListener('keydown', speedController_);
